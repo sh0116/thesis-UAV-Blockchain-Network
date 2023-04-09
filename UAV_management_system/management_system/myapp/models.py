25c4fe58fe0b5b090b572ca4cab29e80d017c879
sh0116 <seokhyeon116@naver.com>
2023-04-09 00:49:20 -0700

    update

--- UAV_management_system/management_system/myapp/models.py
+++ UAV_management_system/management_system/myapp/models.py
@@ -3,9 +3,25 @@ from django.db import models
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    # 유저에 관한 정보
-    # 채널에 대한 정보
-    # 조직에 대한 정보
-    # UAV에 관한 정보
+    organizations = models.TextField()
+    peers = models.TextField()
+    uavs = models.TextField()
+    channels = models.TextField()
+
     def __str__(self):
         return self.user.username
+
+class AuthenticationPeer(models.Model):
+    userprofile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='Authprofile')
+    auth_name = models.TextField()
+    mspid = models.TextField()
+    cryptopath = models.TextField()
+    certpath = models.TextField()
+    keypath = models.TextField()
+    tlscertpath = models.TextField()
+    peerendpoint = models.TextField()
+    gatewaypeer = models.TextField()
+
+
+    def __str__(self):
+        return self.userprofile.user.username
--- UAV_management_system/management_system/myapp/views.py
+++ UAV_management_system/management_system/myapp/views.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.conf import settings
+from django.contrib.auth.models import User
+from .models import UserProfile, AuthenticationPeer
 import json, os, requests
 
-from django.conf import settings
 
 class UAV_LoginView(LoginView):
     template_name = 'login.html'  # 사용자 정의 로그인 템플릿을 지정합니다.
@@ -22,11 +25,57 @@ class UAV_LoginView(LoginView):
         else:
             return render(request, 'login.html', {'error_message': 'Invalid login credentials'})
 
-    
+
+def logout_view(request):
+    logout(request)
+    return redirect('/') 
+
+@login_required(login_url='/login/')
+def updateAuthenticationPeer(request, auth_peer_id):
+    if request.method == 'PUT':
+        try:
+            data = json.loads(request.body)
+            auth_peer = AuthenticationPeer.objects.get(pk=auth_peer_id)
+            print(data)
+            auth_peer.auth_name = data.get('auth_name', auth_peer.auth_name)
+            auth_peer.mspid = data.get('mspid', auth_peer.mspid)
+            auth_peer.cryptopath = data.get('cryptopath', auth_peer.cryptopath)
+            auth_peer.certpath = data.get('certpath', auth_peer.certpath)
+            auth_peer.keypath = data.get('keypath', auth_peer.keypath)
+            auth_peer.tlscertpath = data.get('tlscertpath', auth_peer.tlscertpath)
+            auth_peer.peerendpoint = data.get('peerendpoint', auth_peer.peerendpoint)
+            auth_peer.gatewaypeer = data.get('gatewaypeer', auth_peer.gatewaypeer)
+            
+            auth_peer.save()
+            response_data = {
+                'id': auth_peer.id,
+                'auth_name': auth_peer.auth_name,
+                'mspid': auth_peer.mspid,
+                'cryptopath': auth_peer.cryptopath,
+                'certpath': auth_peer.certpath,
+                'keypath': auth_peer.keypath,
+                'tlscertpath': auth_peer.tlscertpath,
+                'peerendpoint': auth_peer.peerendpoint,
+                'gatewaypeer': auth_peer.gatewaypeer,
+            }
+            return JsonResponse(response_data, status=200)
+        except AuthenticationPeer.DoesNotExist:
+            return JsonResponse({'error': 'AuthenticationPeer not found'}, status=404)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
+
 
 @login_required(login_url='/login/')
 def index(request):
-   return render(request, "index.html")
+    user_profile = UserProfile.objects.get(user=request.user)
+    auth_peers = AuthenticationPeer.objects.filter(userprofile=user_profile)
+    context = {
+        'user': request.user,
+        'auth_peers': auth_peers
+        }
+    return render(request, 'index.html', context)
 
 @login_required(login_url='/login/')
 def dashboard(request):
@@ -34,11 +83,39 @@ def dashboard(request):
     return HttpResponseRedirect('http://'+host+':3000/dashboards')
 
 @login_required(login_url='/login/')
-def client(request):
+def client_uav(request):
     url = "http://192.168.72.130:8000"+"/uav/"
     response = requests.get(url)
     return HttpResponse(json.dumps(response.json()), content_type='application/json')
 
+@login_required(login_url='/login/')
+def client_hlf(request, auth_peer_id):
+    url = "http://192.168.72.128:3030/connect"
+    auth_peer = AuthenticationPeer.objects.get(pk=auth_peer_id)
+
+    requests_data = {
+        "auth" : {
+            'mspID': auth_peer.mspid,
+            'cryptoPath': auth_peer.cryptopath,
+            'certPath': auth_peer.certpath,
+            'keyPath': auth_peer.keypath,
+            'tlsCertPath': auth_peer.tlscertpath,
+            'peerEndpoint': auth_peer.peerendpoint,
+            'gatewayPeer': auth_peer.gatewaypeer,
+        },
+        "asset" : {
+            "ID":"",
+            "Status":"",
+            "Latitude":"",
+            "Longitude":""
+        }
+    }
+    response = requests.get(url, data=json.dumps(requests_data))
+    if response.status_code==200:
+        return HttpResponse(response.status_code, content_type='application/json')
+    else:
+        return HttpResponse(response.status_code, content_type='application/json')
+""
 @login_required(login_url='/login/')
 def history(request):
     return render(request, 'history.html')
@@ -50,4 +127,3 @@ def tracker(request):
 @login_required(login_url='/login/')
 def mission(request, mission_name):
     return render(request, "mission.html")
-#data = {"latitude": 39.904211, "longitude": 115.407395, "uav_manager": "New York"}
